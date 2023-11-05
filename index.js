4ecf277224037e3451775200fc15369a9cf913d0
Coder Mahdi <codermahdihasan@gmail.com>
2023-11-05 13:28:31 +0600

    server connected with mongodb

--- index.js
+++ index.js
@@ -1,9 +1,42 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 7000;
+require("dotenv").config();
+
 app.use(express.json());
 app.use(cors());
+
+console.log(process.env.DB_NAME);
+
+const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.rg5wc51.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const foodCollection = client.db("theDiner").collection("Foods");
+
+    
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    //  await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("The diner running");
